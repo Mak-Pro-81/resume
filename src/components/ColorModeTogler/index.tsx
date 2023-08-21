02636369e81364adcbcc9e0864ded871985bf8c3
MakPro <maks.prokopenko@oneplusone.solutions>
2023-08-21 10:06:15 +0300

    first commit

---
+++ src/components/ColorModeTogler/index.tsx
@@ -0,0 +1,9 @@
+import styles from './ColorModeTogler.module.scss';
+
+export const ColorModeTogler = () => {
+    return (
+        <>
+            <button className={styles.toggler}>Mode</button>
+        </>
+    );
+};
